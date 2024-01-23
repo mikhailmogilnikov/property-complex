7043300c93229ec0976334ac016e84d827b2597c
Mikhail Mogilnikov <mikhail.mogilnikov02@gmail.com>
2024-01-23 15:36:00 +0300

    feat: list items

--- src/constants/icons.jsx
+++ src/constants/icons.jsx
@@ -1,20 +1,31 @@
-import { PiUserCircleBold, PiListMagnifyingGlassBold, PiMapPinBold, PiMagnifyingGlassBold } from "react-icons/pi";
-import { TbMap2 } from "react-icons/tb";
+import {
+  PiUserCircleBold,
+  PiListMagnifyingGlassBold,
+  PiMapPinBold,
+  PiMagnifyingGlassBold,
+  PiCaretRightBold,
+  PiKeyBold,
+  PiPackageBold,
+} from 'react-icons/pi';
+import { TbMap2 } from 'react-icons/tb';
 
 export default {
   menu: {
     header: {
-      location: <PiMapPinBold className="w-4 h-4 flex-shrink-0" />,
+      location: <PiMapPinBold className='w-4 h-4 flex-shrink-0' />,
     },
     content: {
       list: {
-        search: <PiMagnifyingGlassBold className="w-full h-full"/>,
+        search: <PiMagnifyingGlassBold className='w-full h-full' />,
+        caret: <PiCaretRightBold className='w-full h-full' />,
+        owner: <PiKeyBold className='w-full h-full' />,
+        items: <PiPackageBold className='w-full h-full' />,
       },
     },
     nav: {
-      list: <PiListMagnifyingGlassBold className="w-full h-full"/>,
-      locations: <TbMap2 className="w-full h-full"/>,
-      menu: <PiUserCircleBold className="w-full h-7"/>,
-    }
-  }
-}
+      list: <PiListMagnifyingGlassBold className='w-full h-full' />,
+      locations: <TbMap2 className='w-full h-full' />,
+      menu: <PiUserCircleBold className='w-full h-7' />,
+    },
+  },
+};
